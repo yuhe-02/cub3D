4d7650b86039112045d1a4d653182c9a16db0f6e
Yuhei Yamasaki <yyamasak@c3r3s5.42tokyo.jp>
2025-01-26 11:43:02 +0900

    split draw_vertical function

--- cub3D/srcs/math/raycast.c
+++ cub3D/srcs/math/raycast.c
@@ -1,31 +1,6 @@
 #include "utils.h"
 
 
-static void	_draw_vertical_(t_data *data, t_ivec *coord, t_wall *wall)
-{
-	int	color;
-
-	coord->y = 0;
-	while (coord->y < data->img.height)
-	{
-		if (coord->y < wall->draw_start)
-			color = white_16;
-		else if (coord->y < wall->draw_end)
-		{
-			wall->tex.y = (int)wall->tex_pos & (wall->target_img->height - 1);
-			wall->tex_pos += wall->step;
-			// Calculate pixel position in the texture
-			char *tex_pixel = wall->target_img->addr + (wall->tex.y * wall->target_img->llen + wall->tex.x * (wall->target_img->bpp / 8));
-			// Convert texture pixel color (assuming 32-bit color depth)
-			color = *(int *)tex_pixel;
-		}
-		else
-			color = black_16;
-		_ft_mlx_pixel_put(data, coord->x, coord->y, color);
-		coord->y++;
-	}
-}
-
 // DDA algo
 static void _calc_hit_pos_(t_params *params, t_ray *ray)
 {
@@ -139,7 +114,7 @@ void	_raycast(t_params *params)
 		wall.tex_pos = (wall.draw_start - data->img.height / 2 + wall.line_height / 2) * wall.step;
 
 		// contents drawer
-		_draw_vertical_(data, &coord, &wall);
+		_draw_vertical(data, &coord, &wall);
 		coord.x++;
 	}
 }
