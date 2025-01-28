d9f5200176949d1bdd606ecf895a9981f5b1ba35
yuhe-02 <domineter4649@gmail.com>
2025-01-28 15:42:58 +0000

    create

--- cub3D/srcs/parser/parse_color.c
+++ cub3D/srcs/parser/parse_color.c
@@ -2,20 +2,13 @@
 #include "libft.h"
 #include "raycast.h"
 
+// TODO 入力に不備があった場合のバリデーションしてない
 
-//==============================================================
-// parse_color : RGBの値の範囲をチェック
-//==============================================================
-
-int	is_valid_rgb(int value)
+static int	is_valid_rgb(int value)
 {
 	return (value >= 0 && value <= 255);
 }
-//==============================================================
-// parse_color : RGBを16進数に変換
-//==============================================================
-
-int	convert_rgb_hex(int *rgb_colors)
+static int	convert_rgb_hex(int *rgb_colors)
 {
 	int	hex_color;
 	int	i;
@@ -29,11 +22,7 @@ int	convert_rgb_hex(int *rgb_colors)
 	return (hex_color);
 }
 
-//==============================================================
-// parse_color : RGB文字列を分割しint型に変換
-//==============================================================
-
-int	parse_rgb_values(char **rgb, int *rgb_colors)
+static int	parse_rgb_values(char **rgb, int *rgb_colors)
 {
 	int	i;
 
@@ -50,11 +39,7 @@ int	parse_rgb_values(char **rgb, int *rgb_colors)
 	}
 	return (0);
 }
-//==============================================================
-// parse_color : char **開放
-//==============================================================
-
-void free_char_rgb(char **rgb)
+static void free_char_rgb(char **rgb)
 {
 	int	i;
 
@@ -66,31 +51,18 @@ void free_char_rgb(char **rgb)
 	}
 	free(rgb);
 }
-//==============================================================
-// parse_color : main
-//==============================================================
+
 int		parse_color(char *line, int *color)
 {
 	char	**rgb;
 	int		rgb_color[3];
 
-	if (!line)
-		return (-1);
 	rgb = ft_split(line, ',');
-	if (!rgb)
-	{
-		write(2, "Error : split\n", 14);
-		return (-1);
-	}
-	
 	if (parse_rgb_values(rgb, rgb_color) != 0)
 	{
 		free_char_rgb(rgb);
-		return (-1);
+		exit(1);
 	}
-
-	*color = convert_rgb_hex(rgb_color);
-
 	free_char_rgb(rgb);
-	return (0);
+	return (convert_rgb_hex(rgb_color));
 }
