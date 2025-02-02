d477c91acac363f81256483264968fb5cf4ccc53
Yuhei Yamasaki <yyamasak@c2r6s10.42tokyo.jp>
2025-02-02 17:26:28 +0900

    fix uninitialized value

--- cub3D_dir/srcs/parser/check_valid_map.c
+++ cub3D_dir/srcs/parser/check_valid_map.c
@@ -6,7 +6,7 @@
 /*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:13:22 by yyamasak          #+#    #+#             */
-/*   Updated: 2025/02/02 16:01:43 by yyamasak         ###   ########.fr       */
+/*   Updated: 2025/02/02 16:47:54 by yyamasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,23 @@ static	int is_block_(char c)
 	return (c == '0' || c == '1');
 }
 
+// static	int	check_horizontal_char_(char c, char top, char bottom)
+// {
+	
+// }
+
 int _check_valid_map(int len, int start_index, char **line)
 {
 	int	i;
 	int	j;
-	int		space_flg;
 	int		user_dir_flg;
-	int		start_flg;
 	char	current_char;
 	char	next_char;
 	char	above_char;
 	char	bottom_char;
 
 	i = 0;
-	space_flg = 0;
 	user_dir_flg = 0;
-	start_flg = 0;
 	// TODO if 入力予期していないアスキーの場合の処理
 	// TODO if ユーザーの位置が存在しない、または複数候補があるとき
 	// TODO if 壁で囲まれていない場合
@@ -98,7 +99,7 @@ int _check_valid_map(int len, int start_index, char **line)
 			{
 				error_exit("Invalid map: found space bottom\n", 1);
 			}
-
+			// check player is not near space (horizontal check)
 			if (j == 0 && (is_user_dir_(current_char)))
 			{
 				error_exit("Invalid map: player not locate field in left top\n", 1);
@@ -111,7 +112,7 @@ int _check_valid_map(int len, int start_index, char **line)
 			{
 				error_exit("Invalid map: found space before player\n", 1);
 			}
-			// check 0 is not near space (vertical check)
+			// check player is not near space (vertical check)
 			if (i == 0 && is_user_dir_(current_char))
 			{
 				error_exit("Invalid map: player not locate in top\n", 1);
