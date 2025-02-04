fabe81f312383f1b21a6768f656f9a122ac1f472
yuhe-02 <domineter4649@gmail.com>
2025-02-05 00:54:12 +0900

    solve norminette in math fold

--- project/srcs/math/convert_angle.c
+++ project/srcs/math/convert_angle.c
@@ -6,21 +6,21 @@
 /*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 07:01:51 by yyamasak          #+#    #+#             */
-/*   Updated: 2025/02/03 14:22:09 by yyamasak         ###   ########.fr       */
+/*   Updated: 2025/02/05 00:53:57 by yyamasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycast.h"
 
 double	convert_to_radian(int degree)
-{	
+{
 	double	res;
 
 	res = (double)((double)degree / 180 * PI);
 	return (res);
 }
 
-int		convert_to_degree(double radian)
+int	convert_to_degree(double radian)
 {
 	int	res;
 
