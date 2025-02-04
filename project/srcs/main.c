11c4435c1a0b9a948939d4cdbb7ef6cb58339212
yuhe-02 <domineter4649@gmail.com>
2025-02-05 00:55:56 +0900

    solve norm main error.c

--- project/srcs/main.c
+++ project/srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 00:54:54 by yyamasak          #+#    #+#             */
+/*   Updated: 2025/02/05 00:55:39 by yyamasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "raycast.h"
 
 int	main_loop(void *arg)
@@ -17,7 +29,8 @@ int	main_loop(void *arg)
 		mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 	return (0);
 }
-static  int check_all_set_(t_params *params)
+
+static int	check_all_set_(t_params *params)
 {
 	if (!params->data.tex_north.path)
 		return (ft_printf_fd(ERR_FD, "Error\n%s%s\n", ERR2, ERR_NOPATH));
@@ -37,27 +50,30 @@ static  int check_all_set_(t_params *params)
 }
 // static void call_struct(t_params *params)
 // {
-// 	printf("mapwidth, mapheight = (%d, %d)\n", params->map_width, params->map_height);
+// 	printf("mapwidth, mapheight = (%d, %d)\n", params->map_width,
+//		params->map_height);
 // 	printf("path north: %s\n", params->data.tex_north.path);
 // 	printf("path south: %s\n", params->data.tex_south.path);
 // 	printf("path west: %s\n", params->data.tex_west.path);
 // 	printf("path east: %s\n", params->data.tex_east.path);
-// 	printf("ceiling, floor=(%x, %x)\n", params->data.ceilling_color, params->data.floor_color);
-// 	printf("player pos=(%d, %d)\n", params->player.init_userpos_x, params->player.init_userpos_y);
+// 	printf("ceiling, floor=(%x, %x)\n", params->data.ceilling_color,
+//		params->data.floor_color);
+// 	printf("player pos=(%d, %d)\n", params->player.init_userpos_x,
+//		params->player.init_userpos_y);
 // }
+
 int	main(int argc, char **argv)
 {
 	t_params	*params;
 
-  if (argc == 1)
-    error_exit("need arguments\n", 1);
-  if (argc != 2)
-    error_exit("too many arguments\n", 1);
+	if (argc == 1)
+		error_exit("need arguments\n", 1);
+	if (argc != 2)
+		error_exit("too many arguments\n", 1);
 	if (_init_params(&params))
 		exit(1);
 	if (_parse(argv[1], params))
 		exit(1);
-	// call_struct(params);
 	if (check_all_set_(params))
 		exit(1);
 	if (_init_data(&(params->data)))
